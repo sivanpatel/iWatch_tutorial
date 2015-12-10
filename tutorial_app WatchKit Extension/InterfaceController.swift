fb7aacc9754eabadf5d39e10647b1f443d79e5d3
Sivan Patel <sivan.patel@googlemail.com>
2015-12-10 12:20:01 +0000

    Initial Commit

---
+++ tutorial_app WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  tutorial_app WatchKit Extension
+//
+//  Created by Sivan Patel on 10/12/2015.
+//  Copyright © 2015 Sivan Patel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
